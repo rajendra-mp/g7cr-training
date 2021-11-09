c56d8c62f7cd50233ea99906ebe376012b7c8a59
rajendra-mp <rajendra.p@g7cr.in>
2021-11-09 12:06:44 +0530

    Revert "Update index.js"
    This reverts commit ff6659e0bcd4d9dfeb3f782f0c2e728384523f14.

--- codes/day-6/command-line-args/index.js
+++ codes/day-6/command-line-args/index.js
@@ -1,35 +1,6 @@
 // console.log(process.argv.slice(2))
 const dotenv = require('dotenv')
-    //const data = { NAME: 'joydip' }
-    //process.env.NAME='joydip'
+//const data = { NAME: 'joydip' }
+//process.env.NAME='joydip'
 dotenv.config()
 console.log(process.env.NAME)
-
-
-// /*Swagger*/
-// const options = {
-//     swaggerDefinition: {
-//         openapi: '3.0.1',
-//         info: {
-//             title: "Library API",
-//             version: "1.0.0",
-//             description: "A simple Express Library API"
-//         },
-//         basePath: '/',
-//         components: {
-//             securitySchemes: {
-//                 bearerAuth: {
-//                     type: 'http',
-//                     scheme: 'bearer',
-//                     bearerFormat: 'JWT',
-//                 }
-//             }
-//         },
-//         security: [{
-//             bearerAuth: []
-//         }]
-//     },
-//     apis: ["./routes/*.js"]
-// };
-// const specs = swaggerJsDoc(options);
-// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
